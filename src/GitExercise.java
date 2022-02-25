9a39a630bcd25383e061ecf53d4e0e3678275037
AugustG01 <augu8204@stud.kea.dk>
2022-02-25 09:21:20 +0100

    Commit added greet

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class GitExercise {
+
+
+    Scanner in = new Scanner(System.in);
+    String greetings;
+    void greet() {
+        System.out.print("Indtast venligst dit navn: ");
+        greetings = in.nextLine();
+        System.out.println("Hej! "+ greetings);
+
+    }
+
     public static void main(String[] args) {
+
+        new GitExercise().greet();
         System.out.println("It Works!");
     }
 }
